80799ca5d933107f2db0ba04c096fd3cc4e24aca
William Noris <w.noris@protonmail.com>
2022-03-21 10:57:18 +0100

    routing

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,15 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
-
-
+import { AdminRoutingModule } from './admin-routing.module';
 
 @NgModule({
-  declarations: [
-    PageAdminComponent
-  ],
-  imports: [
-    CommonModule
-  ]
+  declarations: [PageAdminComponent],
+  imports: [CommonModule, AdminRoutingModule],
 })
-export class AdminModule { }
+export class AdminModule {}
