3a97bc5d4b706c9fd9c833404171cae3554d7edf
William Noris <w.noris@protonmail.com>
2022-03-21 15:31:11 +0100

    Model...

--- src/app/modules/admin/pages/pages-empty/pages-empty.component.ts
+++ src/app/modules/admin/pages/pages-empty/pages-empty.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { PlantouneService } from 'src/app/services/plantoune.service';
+import { Category } from '../../models/Category';
+import { Plant } from '../../models/plant';
 
 @Component({
   selector: 'app-pages-empty',
   templateUrl: './pages-empty.component.html',
-  styleUrls: ['./pages-empty.component.scss']
+  styleUrls: ['./pages-empty.component.scss'],
 })
 export class PagesEmptyComponent implements OnInit {
-
-  constructor() { }
+  constructor(private plantouneService: PlantouneService) {}
 
   ngOnInit(): void {
+    // this.plantouneService
+    //   .getPlantById('952438')
+    //   .subscribe((result) => console.log(result));
+    // this.plantouneService
+    //   .createPlant(new Plant('maPlante', 200, 5, true, Category.bonzaï, '', 1))
+    //   .subscribe((response) => console.log(response));
+    // this.plantouneService
+    //   .deletePlant('cVTq7Lv')
+    //   .subscribe((response) => console.log(response));
+    // this.plantouneService.getPlantById('QAOVdIF').subscribe((plante: Plant) => {
+    //   let modPlant = plante;
+    //   modPlant.name = 'coucouc je suis une plante modifiée';
+    //   this.plantouneService
+    //     .updatePlant(modPlant)
+    //     .subscribe((response) => console.log(response));
+    // });
   }
-
 }
--- src/app/services/plantoune.service.ts
+++ src/app/services/plantoune.service.ts
@@ -2,23 +2,62 @@ import { Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Plant } from '../modules/admin/models/plant';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PlantouneService {
   plantLiked$ = new Subject<any>();
   apiUrl: string;
-  
+
   constructor(private httpClient: HttpClient) {
     this.apiUrl = environment.apiUrl;
-   }
+  }
+
+  public map(plante: Plant | Object): Object | Plant {
+    if (plante == typeof Plant) {
+      return {
+        id: plante.id,
+        product_name: plante.name,
+        product_price: plante.price,
+        product_instock: plante.instock,
+        product_qty: plante.quantity,
+        product_breadcrumb_label: plante.category,
+        product_url_picture: plante.urlPicture,
+        product_rating: plante.rating,
+      };
+    } else {
+      return new Plant();
+    }
+  }
 
   getData(): Observable<any[]> {
     return this.httpClient.get<any[]>(`${this.apiUrl}/list_products`);
   }
 
   getPlantFav(userId: number): Observable<any[]> {
-    return this.httpClient.get<any[]>(`${this.apiUrl}/favPlant?userId=${userId}`)
+    return this.httpClient.get<any[]>(
+      `${this.apiUrl}/favPlant?userId=${userId}`
+    );
+  }
+
+  getPlantById(plantId: string): Observable<any> {
+    return this.httpClient.get(`${this.apiUrl}/list_products/${plantId}`);
+  }
+
+  createPlant(plant: Plant): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/list_products/`, plant);
+  }
+
+  deletePlant(plantId: string): Observable<any> {
+    return this.httpClient.delete(`${this.apiUrl}/list_products/${plantId}`);
+  }
+
+  updatePlant(plant: Plant): Observable<any> {
+    return this.httpClient.patch(
+      `${this.apiUrl}/list_products/${plant.id}`,
+      plant
+    );
   }
 }
