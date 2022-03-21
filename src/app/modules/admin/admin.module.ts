8d462de4d4119a0cdd66e3c84b6ab70a4598fa29
MohamedKerkeb <kerkeb.moha@gmail.com>
2022-03-21 12:55:46 +0100

    mise en form le formulaire

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { PageAdminComponent } from './pages/page-admin/page-admin.component';
 import { FormComponent } from './components/form/form.component';
 import { AdminRoutingModule } from './admin-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
 	declarations: [PageAdminComponent, FormComponent],
-	imports: [CommonModule, AdminRoutingModule],
+	imports: [CommonModule, AdminRoutingModule, ReactiveFormsModule],
 })
 export class AdminModule {}
--- src/app/modules/admin/components/form/form.component.ts
+++ src/app/modules/admin/components/form/form.component.ts
@@ -1,15 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { Category } from '../../models/Category';
+import { Plant } from '../../models/plant';
 @Component({
-  selector: 'app-form',
-  templateUrl: './form.component.html',
-  styleUrls: ['./form.component.scss']
+	selector: 'app-form',
+	templateUrl: './form.component.html',
+	styleUrls: ['./form.component.scss'],
 })
 export class FormComponent implements OnInit {
+	public plant!: Plant;
+	public categories = Category;
+	public formulaire!: FormGroup;
 
-  constructor() { }
+	constructor(private fp: FormBuilder) {}
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.formulaire = this.fp.group({
+			name: [this.plant.name],
+			price: [this.plant.price],
+			quantity: [this.plant.quantity],
+			instock: [this.plant.instock],
+			category: [this.plant.category],
+			urlPicture: [this.plant.urlPicture],
+			rating: [this.plant.rating],
+			id: [this.plant.id],
+		});
+		console.log(this.categories);
+	}
 
+	public onSubmit(): void {
+		console.log('le formulaire: ', this.formulaire);
+		console.log('Soumis: ', this.formulaire.value);
+	}
 }
